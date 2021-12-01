21308921dbf7886e7ea78f882aca91427cf805a2
Qoosim <ayinde.abdulghaniyy@gmail.com>
2021-12-01 20:44:14 +0100

    Complete the counter project

--- 2-counter/app.js
+++ 2-counter/app.js
@@ -1,15 +1,27 @@
 // set initial count
-
 let count = 0
 
 // select value and buttons
-const value = document.querySelector('#value')
-const btns = document.querySelectorAll('.btn')
 
-btns.forEach(function(btn) {
-    btn.addEventListener('click', function(e) {
-        console.log(e.currentTarget)
-    })
+const value = document.querySelector('#value');
+const btns = document.querySelectorAll('.btn');
+
+btns.forEach((btn) => {
+  btn.addEventListener('click', (e) => {
+    if (e.target.classList.contains('increase')) {
+      count ++;
+    } else if (e.target.classList.contains('decrease')) {
+      count --;
+    } else {
+      count = 0;
+    }
+    if (count > 0) {
+      value.style.color = 'green';
+    } else if (count < 0) {
+      value.style.color = 'red';
+    } else {
+      value.style.color = 'black';
+    }
+    value.textContent = count;
+  })
 })
-
-
